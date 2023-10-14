7ba57fc4b14b19a74909d6aa406609b62cdf4d28
nilaw2017 <nilaw.manandhar2016@gmail.com>
2023-10-14 13:32:43 +0545

    [FE]:: Header menu setup

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1 className="text-red-500">SignIn</h1>
+    </div>
+  );
+}
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignOut() {
+  return (
+    <div>
+      <h1 className="text-red-500">SIGNUP</h1>
+    </div>
+  );
+}
