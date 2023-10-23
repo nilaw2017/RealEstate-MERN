8fc0124815178a9bb6fa987ad2eb6abd248e79ca
nilaw2017 <nilaw.manandhar2016@gmail.com>
2023-10-23 18:38:34 +0545

    [BE]:: MongoDB connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected :)");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
